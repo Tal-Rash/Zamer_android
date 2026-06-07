4b9aed891a8a0fcd62fcc38f6732ca54ef5c0b77
Tal-Rash <shvarchkovsergey@gmail.com>
2026-06-07 14:19:08 +0300

    Remove startup database seeding

--- app/src/main/java/ru/depo/zamerykp/ZameryKpApp.kt
+++ app/src/main/java/ru/depo/zamerykp/ZameryKpApp.kt
@@ -2,17 +2,12 @@ package ru.depo.zamerykp
 
 import android.app.Application
 import ru.depo.zamerykp.data.db.AppDatabase
-import ru.depo.zamerykp.data.db.DatabaseSeeder
 import ru.depo.zamerykp.data.repository.BackupRepository
 import ru.depo.zamerykp.data.repository.ExportRepository
 import ru.depo.zamerykp.data.repository.LocomotiveRepository
 import ru.depo.zamerykp.data.repository.MeasurementRepository
 import ru.depo.zamerykp.data.repository.SettingsRepository
 import ru.depo.zamerykp.data.repository.ServerSyncRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
 
 class ZameryKpApp : Application() {
     lateinit var container: AppContainer
@@ -21,9 +16,6 @@ class ZameryKpApp : Application() {
     override fun onCreate() {
         super.onCreate()
         val db = AppDatabase.create(this)
-        CoroutineScope(SupervisorJob() + Dispatchers.IO).launch {
-            DatabaseSeeder(db).seedIfEmpty()
-        }
         val locomotiveRepository = LocomotiveRepository(db.locomotiveDao(), db.wheelPairProfileDao())
         val measurementRepository = MeasurementRepository(db.measurementDao(), db.locomotiveDao(), db.wheelPairProfileDao())
         val exportRepository = ExportRepository(db.measurementDao(), db.locomotiveDao(), db.wheelPairProfileDao())
